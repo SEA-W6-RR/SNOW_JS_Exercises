237cd98c5416572e65bc5447adade4ac33a93cba
SEA-W6-RR <90201728+SEA-W6-RR@users.noreply.github.com>
2022-07-21 08:05:28 +0200

    SNOW_JS_exercise1_ChapterProject

---
+++ ex1_ChapterProject_alert.js
@@ -0,0 +1,4 @@
+alert("Saying hi from exercise1ChapterProject");
+// This is a
+// multiline 
+// comment
---
+++ ex1_ChapterProject_console.js
@@ -0,0 +1,7 @@
+let a = "Reinhard Riese";
+console.log(a); //value of"a" will be displayed in the console
+// This is a
+// multiline 
+// comment
+let b = Math.floor(Math.random() * 100);
+console.log(b); //value of"b" will be displayed in the console
