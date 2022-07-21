218408c0fa5c547f974ccfa6740fca64d91c2fa7
SEA-W6-RR <90201728+SEA-W6-RR@users.noreply.github.com>
2022-07-21 08:10:20 +0200

    SNOW_JS_exercise4_3

---
+++ exercise4_3.js
@@ -0,0 +1,8 @@
+// Exercise4_3
+//
+age = window.prompt("What's your age?");
+console.log(age, typeof(age));
+age = Number(age);
+console.log(typeof(age));
+message = String;
+age < 21 ?  message = "Sorry, you\'ll not get drunk here!" : message = "Please drink as much alcohol as you like!"
